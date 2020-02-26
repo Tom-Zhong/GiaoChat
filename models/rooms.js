b9545200dcdae948e436d184b124721f43c0c9d5
Dagui <814374557@qq.com>
2020-02-26 13:00:54 +0800

    修改Room的Modal.

--- models/rooms.js
+++ models/rooms.js
@@ -9,7 +9,7 @@ const RoomsSchema = mongoose.Schema(
       ref: 'User',
       required: true
     },
-    onwerId: {                                                         // 房间所有人
+    ownerId: {                                                         // 房间所有人
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
       required: true
@@ -17,7 +17,8 @@ const RoomsSchema = mongoose.Schema(
     title: { type: String, required: true },                           // 房间名称
     desc: { type: String },                                            // 房间描述
     type: { type: Number, required: true, default: 0 },                // 房间类型， 0代表两人聊天房间, 1代表群聊房间
-    members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],  // 房间成员， 创建房间时，会为所有人都创建
+    member: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },     // 房间成员，创建房间时，会为所有拥有这个聊天房间的人都创建，举个例子，有房间有10个成员，就会创建10个房间聊天记录表
+    unReadMessageCount: { type: Number, required: true, default: 0 },  // 没有阅读的信息量
     createTime: {                                                      // 创建时间
       type: Date,
       default: Date.now
