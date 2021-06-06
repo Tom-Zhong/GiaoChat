bb21091e230be35a123a630d60e43a29e7b46bf7
Tom Zhong <814374557@qq.com>
2021-06-06 09:41:49 +0800

    增加注册路由

--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,30 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res,) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/chatroom', function(req, res, next) {
+router.get('/chatroom', function(req, res,) {
   res.render('chatroom', { title: 'chat_room' });
 });
 
-router.get('/rooms', function(req, res, next) {
+router.get('/rooms', function(req, res,) {
   res.render('rooms', { title: 'rooms' });
 });
 
-router.get('/about', function(req, res, next) {
+router.get('/about', function(req, res,) {
   res.render('about', { title: 'about' });
 });
 
-router.get('/signup', function(req, res, next) {
+router.get('/signin', function(req, res,) {
+  res.render('signin', { title: 'signin' });
+});
+router.get('/signup', function(req, res,) {
   res.render('signup', { title: 'signup' });
 });
 
-router.get('/main', function(req, res, next) {
+router.get('/main', function(req, res,) {
   res.render('main', { title: 'main' });
 });
 
