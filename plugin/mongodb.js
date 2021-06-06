54c280b7e07f6c668ac5f4d971991d0c7ef797c0
Tom Zhong <814374557@qq.com>
2021-06-06 08:46:17 +0800

    将MongoDB配置改为从环境变量引入

--- plugin/mongodb.js
+++ plugin/mongodb.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost/zguiyong', {
+mongoose.connect(process.env.MongoDBURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
